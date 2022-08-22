2dd1255fb2573a8a1e770843b5194f52dd8ee206
NayosX <nayos144@gmail.com>
2022-08-22 14:50:30 -0600

    primeras funciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'intership';
+
+
+
+  public isShowMessage:boolean = false;
+
+  public handlerBtn():void {
+    console.log("hola mundo");
+
+    this.isShowMessage = !this.isShowMessage;
+  }
 }
